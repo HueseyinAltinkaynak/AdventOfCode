7c5c087e513933e0959d6f84dffad5abd828b6e7
HueseyinAltinkaynak <huseyin.isparta@hotmail.de>
2024-12-04 02:12:52 +0100

    day1

---
+++ day1/solution.py
@@ -0,0 +1,31 @@
+file = open("./day1/input.txt", "r")
+content = file.read()
+file.close()
+lines = content.split("\n")
+
+total = 0
+total2 = 0
+
+l1 = []
+l2 = []
+
+for line in lines:
+    s = line.split("   ")
+    l1.append(int(s[0]))
+    l2.append(int(s[1]))
+
+l1.sort()
+l2.sort()
+
+for i in range(len(l1)):
+    total += abs(l1[i]-l2[i])
+    for j in range(len(l2)):
+        if (l1[i] == l2[j]):
+            total2 += l1[i]
+        elif (l1[i] < l2[j]):
+            break
+
+
+
+print(total)
+print(total2)
