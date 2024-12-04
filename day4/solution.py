db42d0c7f8584bba91e6553544bd0e7852e431d4
HueseyinAltinkaynak <huseyin.isparta@hotmail.de>
2024-12-04 20:33:45 +0100

    day 4 fertig

--- day4/solution.py
+++ day4/solution.py
@@ -1,7 +1,7 @@
 import sys
 import re
 
-file = open("./day4/testinput.txt", "r")
+file = open("./day4/input.txt", "r")
 content = file.read()
 file.close()
 
@@ -9,12 +9,10 @@ lines = content.split("\n")
 
 directions = [(-1,-1),(-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1)]
 
-maxLine = len(lines)-1
-maxPos = len(lines[0])-1
+maxLine = len(lines)
+maxPos = len(lines[0])
 
 total = 0
-
-print(lines[maxLine][maxPos])
 
 for line in range(maxLine):
   for pos in range(maxPos):
@@ -22,12 +20,26 @@ for line in range(maxLine):
       for dir in directions:
         vlim = line + 3* dir[0]
         hlim = pos + 3* dir[1]
-        if (vlim < 0 or vlim > maxLine or hlim < 0 or hlim > maxPos):
-          test = 0
+        if (vlim < 0 or vlim > maxLine-1 or hlim < 0 or hlim > maxPos-1):
+          continue
         else:
           if (lines[line + dir[0]][pos + dir[1]] == "M"):
             if (lines[line + 2*dir[0]][pos + 2*dir[1]] == "A"):
               if (lines[line + 3*dir[0]][pos + 3*dir[1]] == "S"):
                 total += 1
 
-print(total)
+total2 = 0
+
+for line in range(maxLine-1):
+  if (line == 0):
+    continue
+  for pos in range(maxPos-1):
+    if (pos == 0):
+      continue
+    if(lines[line][pos] == "A"):
+      if (((lines[line-1][pos-1] == "M" and lines[line+1][pos+1] == "S") or (lines[line-1][pos-1] == "S" and lines[line+1][pos+1] == "M")) and ((lines[line-1][pos+1] == "M" and lines[line+1][pos-1] == "S") or (lines[line-1][pos+1] == "S" and lines[line+1][pos-1] == "M"))):
+        total2 += 1
+
+
+print("Part 1: ", total)
+print("Part 2: ", total2)
