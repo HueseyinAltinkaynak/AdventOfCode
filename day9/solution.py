8b6cd42f04faccbb66b094821951a664c012ceb4
Hüseyin Altinkaynak <hueseyin.altinkaynak@promatis.de>
2024-12-10 18:03:30 +0100

    update

--- day9/solution.py
+++ day9/solution.py
@@ -1,7 +1,7 @@
 import sys
 import re
 
-file = open("./day9/input.txt", "r")
+file = open("./day9/testinput.txt", "r")
 content = file.read()
 file.close()
 
@@ -10,9 +10,13 @@ index = 0
 
 compact = ""
 
+compactList = []
+
 for char in content:
     if(file):
         compact += str(index) * int(char)
+        for i in range(int(char)):
+            compactList.append(index)
         index += 1
     else:
         compact += "." * int(char)
@@ -22,20 +26,30 @@ index = 0
 
 # Fehler da id nicht mehr einstellig
 
+finalList = []
+
 while (index < len(compact)):
     if(compact[index] == "."):
+        lastList = compactList.pop()
+        finalList.append(lastList)
         last = compact[len(compact)-1]
         while(last == "."):
             compact = compact[:len(compact)-1]
             last = compact[len(compact)-1]
         compact = compact[:index] + last + compact[index+1:len(compact)-1]
+    else:
+        finalList.append(int(compact[index]))
     index += 1
 
 index = 0
 total1 = 0
 
-for index in range(len(compact)):
-    total1 += index * int(compact[index])
+# for index in range(len(compact)):
+#     total1 += index * int(compact[index])
 
+for i in range(len(finalList)):
+    total1 += i * finalList[i]
 
 print(total1)
+# print(compact)
+# print(finalList)
